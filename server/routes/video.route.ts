8ba6c1176d3a8f01bbdbc1ef8da7e61f0c79a331
Jose Enoc Mtz <josnocpp@gmail.com>
2020-04-09 16:16:48 -0500

    Fixed path issues in video route.

--- server/routes/video.route.ts
+++ server/routes/video.route.ts
@@ -1,6 +1,7 @@
 import * as Router from "koa-router";
 import * as send from "koa-send";
 import * as fs from "fs";
+import { resolve } from "path";
 import { options } from "../main";
 import { db } from "../db";
 
@@ -30,13 +31,13 @@ interface MovieRequest {
 }
 
 function getVideoStream(path: string, videoRange: string) {
-  const stat = fs.statSync(path);
+  const stat = fs.statSync(resolve(path));
   const fileSize = stat.size;
   const range = videoRange;
   return {
     path: path,
     fileSize: fileSize,
-    range: range
+    range: range,
   } as VideoStream;
 }
 
@@ -50,12 +51,12 @@ function getStreamChunk(videoStream: VideoStream) {
   return {
     start: start,
     end: end,
-    size: size
+    size: size,
   } as VideoChunk;
 }
 
 const getVideo = async (router: Router) => {
-  router.get("/video/:id*", async ctx => {
+  router.get("/video/:id*", async (ctx) => {
     const id = ctx.params.id as string;
     try {
       if (id) {
@@ -68,16 +69,14 @@ const getVideo = async (router: Router) => {
           const streamChunk = getStreamChunk(videoStream);
           const movie = fs.createReadStream(videoStream.path, {
             start: streamChunk.start,
-            end: streamChunk.end
+            end: streamChunk.end,
           });
 
           const header = {
-            "Content-Range": `bytes ${streamChunk.start}-${streamChunk.end}/${
-              videoStream.fileSize
-            }`,
+            "Content-Range": `bytes ${streamChunk.start}-${streamChunk.end}/${videoStream.fileSize}`,
             "Accept-Ranges": "bytes",
             "Content-Length": streamChunk.size,
-            "Content-Type": "video/mp4"
+            "Content-Type": "video/mp4",
           };
 
           ctx.res.writeHead(206, header);
@@ -85,7 +84,7 @@ const getVideo = async (router: Router) => {
         } else {
           const header = {
             "Content-Length": videoStream.fileSize,
-            "Content-Type": "video/mp4"
+            "Content-Type": "video/mp4",
           };
 
           ctx.res.writeHead(206, header);
@@ -99,7 +98,7 @@ const getVideo = async (router: Router) => {
         console.error(JSON.stringify(e));
         ctx.throw({
           state: 500,
-          error: { message: "Internal Error", code: 100 }
+          error: { message: "Internal Error", code: 100 },
         });
       } else {
         console.log(e);
